f88d73e79e1accb5c67d60170cc3d237aa58d07e
junbai <Administrator@BF-20181210FDXD>
2019-03-21 00:27:13 +0800

    提交到本地

---
+++ src/main/java/com/quanxian/pojo/Result.java
@@ -0,0 +1,41 @@
+package com.quanxian.pojo;
+
+import java.util.List;
+
+public class Result {
+	
+	private Boolean status;
+	
+	private User user;
+	
+	private List<User> list;
+
+	
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<User> getList() {
+		return list;
+	}
+
+	public void setList(List<User> list) {
+		this.list = list;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/quanxian/pojo/User.java
@@ -0,0 +1,57 @@
+package com.quanxian.pojo;
+
+public class User {
+	
+	private Integer id;
+	
+	private String username;
+	
+	private String mingcheng;
+	
+	private String password;
+	
+	private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getMingcheng() {
+		return mingcheng;
+	}
+
+	public void setMingcheng(String mingcheng) {
+		this.mingcheng = mingcheng;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
